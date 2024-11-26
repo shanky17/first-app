b2b8484608325e8d141fda4987f2f695179c3c6a
shanky17 <shashaank.soni@gmail.com>
2024-11-26 20:11:41 +0530

    Added HttpClient to the project

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CoursesComponent } from '../courses/courses.component';
 import { AboutComponent } from '../about/about.component';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,30 @@ import { AboutComponent } from '../about/about.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private http = inject(HttpClient);
+
+  ngOnInit() {
+    this.fetchHttpData();
+  }
+
+  async fetchHttpData() {
+    // this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe({
+    //   next: (posts) => {
+    //     console.log(posts);
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //   },
+    // });
+
+    try {
+      const posts = await lastValueFrom(
+        this.http.get('https://jsonplaceholder.typicode.com/posts')
+      );
+      console.log(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
